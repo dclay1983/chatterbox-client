73418be7600581ed291aaf5a810e1ca268e3c0e5
Michelene Chon <michelenechon@gmail.com>
2019-11-02 12:00:47 -0700

    Implement Parse method filterInput()

--- client/scripts/parse.js
+++ client/scripts/parse.js
@@ -1,9 +1,25 @@
+// Parse locally stores information which:
+// - Messages cares about
+// - Rooms cares about
+//
 var Parse = {
 
   server: `http://parse.${window.CAMPUS}.hackreactor.com/chatterbox/classes/messages`,
 
   create: function(message, successCB, errorCB = null) {
     // todo: save a message to the server
+    $.ajax({
+      url: Parse.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function(data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: errorCB || function(error) {
+        console.error('chatterbox: Failed to fetch messages', error);
+      }
+    });
   },
 
   readAll: function(successCB, errorCB = null) {
@@ -17,6 +33,12 @@ var Parse = {
         console.error('chatterbox: Failed to fetch messages', error);
       }
     });
-  }
+  },
 
+  filterInput: function(data) {
+    // examine the response from the server request:
+    // For now, just print it to console
+    // Later we will use underbar templating (maybe?) to filter out any potentially bad stuff
+    console.log(data);
+  }
 };
